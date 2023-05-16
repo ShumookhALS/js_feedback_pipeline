84a2acbf1ca40241808fc2772da93a6aa0435a50
Shumookh.ALS <esalshehhi@gmail.com>
2023-05-16 12:30:03 +0400

    update

--- JS1.js
+++ JS1.js
@@ -1,1 +1,34 @@
-console.log("This is log")
+// //console.log("Hello World")
+//  console.log( typeof numberOfStudents)
+// var numberOfStudents =25; 
+// console.log( typeof numberOfStudents)
+// numberOfStudents = 99; 
+// console.log (typeof numberOfStudents)
+// numberOfStudents = "fd"; 
+// console.log(typeof numberOfStudents)
+
+
+
+// var x =30; 
+// console.log( typeof x)
+
+// x= "sh"; 
+// console.log( typeof x)
+
+// x = null; 
+// console.log( typeof x)
+
+
+var myname = "shumookh"; 
+console.log( typeof myname);
+var myage= 24; 
+console.log( typeof myage);
+console.log( myname[0]);
+console.log(myname.charAt(0));
+console.log( myname.indexOf("s")); 
+
+var longString = "This is long string";
+var isEndWith = longString.endsWith("long string");
+
+console.log("IsEndWith: " , isEndWith); 
+console.log("String length" , myname.length); 
